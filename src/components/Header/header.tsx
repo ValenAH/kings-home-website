1e82e8849e9ba1d290b8cce8ca088f4624f2a56a
ValenAH <88941402+ValenAH@users.noreply.github.com>
2026-03-06 21:25:31 -0500

    Update header.tsx

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -3,25 +3,25 @@ import { BedDouble, ShoppingCart } from "lucide-react"
 
 export function Header() {
   return (
-    <header className="px-4 lg:px-6 h-14 flex items-center bg-black">
+    <header className="px-8 md:px-12 lg:px-16 xl:px-20 h-16 flex items-center bg-transparent absolute top-0 left-0 right-0 z-50">
       <Link className="flex items-center justify-center" href="/">
-        <BedDouble className="h-6 w-6 mr-2 text-blue-300" />
-        <span className="font-bold text-blue-300">HOGAR DE REYES</span>
+        <BedDouble className="h-6 w-6 mr-2 text-white" />
+        <span className="text-white font-[family-name:var(--font-abril-fatface)] text-xl">Hogar de Reyes</span>
       </Link>
-      <nav className="ml-auto flex gap-4 sm:gap-6">
-        <Link className="text-sm font-medium text-blue-300 hover:text-dreamgold" href="/">
+      <nav className="ml-auto flex gap-4 sm:gap-6 font-[family-name:var(--font-outfit)] uppercase">
+        <Link className="text-sm font-normal text-[#C8C6C6] hover:text-dreamgold" href="/">
           Inicio
         </Link>
-        <Link className="text-sm font-medium text-blue-300 hover:text-dreamgold" href="/customize">
+        <Link className="text-sm font-normal text-[#C8C6C6] hover:text-dreamgold" href="/customize">
           Personalizar
         </Link>
-        <Link className="text-sm font-medium text-blue-300 hover:text-dreamgold" href="/allies">
+        <Link className="text-sm font-normal text-[#C8C6C6] hover:text-dreamgold" href="/allies">
           Aliados
         </Link>
-        <Link className="text-sm font-medium text-blue-300 hover:text-dreamgold" href="/#contact">
+        <Link className="text-sm font-normal text-[#C8C6C6] hover:text-dreamgold" href="/#contact">
           Contacto
         </Link>
-        <Link className="text-sm font-medium text-blue-300 hover:text-dreamgold flex items-center" href="/shopping-cart">
+        <Link className="text-sm font-normal text-[#C8C6C6] hover:text-dreamgold flex items-center" href="/shopping-cart">
           <ShoppingCart className="h-5 w-5" />
           <span className="sr-only">Carrito</span>
         </Link>
@@ -29,4 +29,3 @@ export function Header() {
     </header>
   )
 }
-
